3d16c158b85bb9c5734620ff8b6112c1d2eddb8b
Adrien-Ray <forpub@accesdenied.net>
2023-03-06 16:54:52 +0100

    layout

--- src/components/layout/Layout.jsx
+++
@@ -1,9 +0,0 @@
-import './layout.css';
-
-function Layout() {
-    return (
-        <h1>coucou!</h1>
-    )
-}
-
-export default Layout;
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Layout from './components/layout/Layout.jsx';
+import './layout.css';
+// import Layout from './components/layout/Layout.jsx';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function funcFetch(updatebrutData) {
+    console.log('il faut fetch');
+    var url = 'https://portfolio.accesdenied.net/api';
+    var header = {
+        mode: 'no-cors',
+        method: "GET",
+        header: {
+            /* 'Accept': 'application/json', */
+            'Content-Type': ' application/json'
+        },
+    };
+    fetch(url, header)
+        .then(response => JSON.stringify(response.json()))
+        .then(json => {
+            console.log(json);
+        })
+}
+
+function Layout() {
+    const [brutData, updatebrutData] = React.useState({});
+    let testExistData = Object.keys(brutData).length;
+    console.log(testExistData);
+    if (testExistData === 0) {
+        funcFetch(updatebrutData);
+    } else {
+        console.log('le fetch a été effectuer');
+    }
+    return (
+        <h1>coucou!</h1>
+    )
+}
+
 root.render(
     <React.StrictMode >
         <Layout />
