d727eafa23ab9b5299baee24e2792a5725722e82
Adrien-Ray <forpub@accesdenied.net>
2023-03-09 17:10:17 +0100

    ??

--- src/components/ProjectList/ProjectList.jsx
+++ src/components/ProjectList/ProjectList.jsx
@@ -1,17 +1,23 @@
 import Card from "../Card/Card";
+import CardSlide from "../CardSlide/CardSlide";
 import './ProjectList.css';
 
 function ProjectList(props) {
+    let projectArray = props.brutData.project;
+    projectArray.sort((b, a) => {
+        return a.project_id - b.project_id;
+      });
+    console.log('props.brutData.project.reverse() : ',  projectArray);
     let list = [];
-    for (let index = 0; index < props.brutData.project.length; index++) {
-        const element = props.brutData.project[index];
-        list.push(
-            <Card type="project" object={element}/>
-        )
+    for (let index = 0; index < projectArray.length; index++) {
+        const element = projectArray[index];
+            list.push(
+                <Card type="project" object={element}/>
+            )
     }
     return (
         <div className="ProjectList">
-            <h1>ProjectList</h1>
+            <CardSlide list={projectArray}/>
             <div className="ProjectList__parentCard">
                 {list}
             </div>
