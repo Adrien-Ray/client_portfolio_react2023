1a17747e109fe81faac817ffb292783d05d0ec38
Adrien-Ray <forpub@accesdenied.net>
2023-03-09 09:39:23 +0100

    cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,17 +1,41 @@
 import './Card.css';
 
+const folderToUpload = 'https://portfolio.accesdenied.net/assets/img/upload/'
+
 function Card(props) {
     // props: type (project or article) and object
     let title;
+    let link;
+    let target;
+    let imgSrc;
+    let classThumb;
     if (props.type === "project") {
         title = props.object.project_title;
+        if (props.object.project_link === "") {
+            link = "#";
+            target = "";
+        } else {
+            link = props.object.project_link;
+            target = "_blank";
+        }
+        imgSrc = folderToUpload+props.object.project_thumbnail;
+        if (props.object.project_thumbnail_fit === 0) {
+            classThumb = "";
+        } else {
+            classThumb = "contain";
+        }
     } else {
         
     }
     return (
-            <div className="Card">
+            <a href={link} target={target} className="Card">
                 <h2>{title}</h2>
-            </div>
+                <img
+                    src={imgSrc}
+                    alt={'thumbnail du projet'+title}
+                    className={classThumb}
+                />
+            </a>
     )
 }
 
