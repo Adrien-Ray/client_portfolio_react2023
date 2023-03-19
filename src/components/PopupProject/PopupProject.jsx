7d90f380bec3e589233ea9cd56a7d7fcb67d10bf
Adrien-Ray <forpub@accesdenied.net>
2023-03-19 13:44:57 +0100

    popup is ready to write2

--- src/components/PopupProject/PopupProject.jsx
+++ src/components/PopupProject/PopupProject.jsx
@@ -1,5 +1,5 @@
 import "./PopupProject.css";
-import React, { useState/* , useEffect */ } from 'react';
+// import React, { useState/* , useEffect */ } from 'react';
 
 function PopupProject (props) {
     // props : object={props.object} showPopup={showPopup} setShowPopup={setShowPopup}
