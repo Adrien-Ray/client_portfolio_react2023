4ee1aaa34e251932fa334a58505a132bfa6716de
Adrien-Ray <forpub@accesdenied.net>
2023-03-20 14:56:50 +0100

    finish opening animations

--- src/components/Presentation/Presentation.jsx
+++ src/components/Presentation/Presentation.jsx
@@ -4,8 +4,14 @@ function Presentation() {
     return (
         <div className="presentation">
             <div className="presentation__center">
-                <h1>Bienvenu sur mon portfolio</h1>
+                <h1><img /* src={process.env.PUBLIC_URL + '/logo192.png'} */ src='https://portfolio.accesdenied.net/assets/ico/logo-perso.png' alt="" /> Bienvenu sur mon portfolio <img /* src={process.env.PUBLIC_URL + '/logo192.png'} */ src='https://portfolio.accesdenied.net/assets/ico/logo-perso.png' alt="" className='endImg'/></h1>
                 <p>Ceci est un client web connecter à l'API du portfolio initial.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, minus! Quo sed numquam enim ullam accusantium iste ratione, rerum dicta, est recusandae, corrupti possimus libero quis ab dolores perspiciatis dignissimos.
+                Totam ex libero similique ullam ipsa esse culpa, nam alias quam dolores perferendis quos? Maxime illum esse eveniet quasi laborum consectetur? Excepturi placeat quo incidunt autem! Magnam, molestiae ratione. Quasi!
+                Ducimus quia voluptate ipsam error repudiandae sunt facere quis voluptatum totam aliquam iste dolor non magni suscipit nesciunt, porro cum similique amet deleniti beatae alias. Magnam laudantium eos fugit autem.
+                Vel ipsum ullam molestiae. Perspiciatis, accusantium? Architecto commodi sint possimus quam itaque. Soluta laboriosam deleniti voluptatum officiis modi mollitia rem quisquam debitis et recusandae tempore consequatur, vel eaque esse illo?
+                Odio, laudantium a? Architecto molestiae vel cumque earum inventore itaque debitis tempore, illo odio natus sunt quia assumenda modi quod at nisi mollitia aut fugit blanditiis laudantium. Maiores, exercitationem nam?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam vel esse explicabo. Pariatur at eos nisi alias mollitia veritatis nesciunt vitae vero tenetur voluptatibus aliquid nam, nulla distinctio quas? Ipsa, officia. Quae nulla non cumque exercitationem reiciendis facilis soluta molestiae.</p>
                 <div className="presentation__center__layer"></div>
             </div>
         </div>
