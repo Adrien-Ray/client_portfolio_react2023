1bcd1737dace1678c1a640918d719a407d589b38
Adrien-Ray <forpub@accesdenied.net>
2023-03-20 17:19:00 +0100

    finish opening animations3

--- src/components/Presentation/Presentation.jsx
+++ src/components/Presentation/Presentation.jsx
@@ -5,13 +5,16 @@ function Presentation() {
         <div className="presentation">
             <div className="presentation__center">
                 <h1><img /* src={process.env.PUBLIC_URL + '/logo192.png'} */ src='https://portfolio.accesdenied.net/assets/ico/logo-perso.png' alt="" /> Bienvenu sur mon portfolio <img /* src={process.env.PUBLIC_URL + '/logo192.png'} */ src='https://portfolio.accesdenied.net/assets/ico/logo-perso.png' alt="" className='endImg'/></h1>
-                <p>Ceci est un client web connecter à l'API du portfolio initial.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, minus! Quo sed numquam enim ullam accusantium iste ratione, rerum dicta, est recusandae, corrupti possimus libero quis ab dolores perspiciatis dignissimos.
-                Totam ex libero similique ullam ipsa esse culpa, nam alias quam dolores perferendis quos? Maxime illum esse eveniet quasi laborum consectetur? Excepturi placeat quo incidunt autem! Magnam, molestiae ratione. Quasi!
-                Ducimus quia voluptate ipsam error repudiandae sunt facere quis voluptatum totam aliquam iste dolor non magni suscipit nesciunt, porro cum similique amet deleniti beatae alias. Magnam laudantium eos fugit autem.
-                Vel ipsum ullam molestiae. Perspiciatis, accusantium? Architecto commodi sint possimus quam itaque. Soluta laboriosam deleniti voluptatum officiis modi mollitia rem quisquam debitis et recusandae tempore consequatur, vel eaque esse illo?
-                Odio, laudantium a? Architecto molestiae vel cumque earum inventore itaque debitis tempore, illo odio natus sunt quia assumenda modi quod at nisi mollitia aut fugit blanditiis laudantium. Maiores, exercitationem nam?</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam vel esse explicabo. Pariatur at eos nisi alias mollitia veritatis nesciunt vitae vero tenetur voluptatibus aliquid nam, nulla distinctio quas? Ipsa, officia. Quae nulla non cumque exercitationem reiciendis facilis soluta molestiae.</p>
+                <p>Ceci est un client web connecter à l'API du portfolio initial. Vous pouvez consulter le portfolio d'origine via le lien ci dessous:</p>
+                <a className='presentation__buttonLink' href='https://portfolio.accesdenied.net' target="_blank" rel='noreferrer'>
+                    <div className="backLayer"></div>
+                    <div className="content">lien vers le portfolio initial</div>
+                </a>
+                <p>Vous pouvez me contacter via le formulaire de contact:</p>
+                <a className='presentation__buttonLink' href='https://portfolio.accesdenied.net/pages/apropos.php' target="_blank" rel='noreferrer'>
+                    <div className="backLayer"></div>
+                    <div className="content">Contact</div>
+                </a>
                 <div className="presentation__center__layer"></div>
             </div>
         </div>
