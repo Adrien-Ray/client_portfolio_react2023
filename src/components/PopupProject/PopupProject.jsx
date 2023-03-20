c378a345f5c83bdbe752a965afbdc48bef66a2a9
Adrien-Ray <forpub@accesdenied.net>
2023-03-20 10:28:40 +0100

    popupProject condition to display childs elements

--- src/components/PopupProject/PopupProject.jsx
+++ src/components/PopupProject/PopupProject.jsx
@@ -11,6 +11,55 @@ function PopupProject (props) {
         console.log('showPopup', props.showPopup);
     }
 
+    let displayPtech = "";
+    if (props.object.project_specification_tech !== "") {
+        displayPtech = (
+            <div className="PopupProject__specification_tech">
+                <h3>Spécifications techniques</h3>
+                <p>{props.object.project_specification_tech}</p>
+            </div>
+        );
+    }
+
+    let displayPfunc = "";
+    if (props.object.project_specification_function !== "") {
+        displayPfunc = (
+            <div className="PopupProject__specification_function">
+                <h3>Spécifications fonctionnels</h3>
+                <p>{props.object.project_specification_function}</p>
+            </div>
+        );
+    }
+
+    let dateBegin = "";
+    if (props.object.project_begin !== "") {
+        dateBegin = <p className="date">Debut du projet: {props.object.project_begin}</p>
+    }
+
+    let dateEnd = "";
+    if (props.object.project_end !== ("" || null)) {
+        dateEnd = <p className="date">Fin du projet: {props.object.project_end}</p>
+    }
+
+    let linkRepo = "";
+    if (props.object.project_github !== "") {
+        linkRepo = (
+        <a className='PopupProject__buttonLink' href={props.object.project_github}>
+            <div className="backLayer"></div>
+            <div className="content">lien repository</div>
+        </a>
+        );
+    }
+    let linkProject = "";
+    if (props.object.project_link !== "") {
+        linkProject = (
+        <a className='PopupProject__buttonLink' href={props.object.project_link}>
+            <div className="backLayer"></div>
+            <div className="content">lien projet</div>
+        </a>
+        );
+    }
+
     return (
         <div className={parentClass}>
 
@@ -20,8 +69,16 @@ function PopupProject (props) {
             </div>
 
             <h2 className="PopupProject__title">PopupProject {props.object.project_title}</h2>
+            <div className="PopupProject__links">
+                {linkRepo}
+                {linkProject}
+            </div>
+            {dateBegin}
+            {dateEnd}
             <img className="PopupProject__thumbnail" src={props.folderToUpload+props.object.project_thumbnail} alt="présentation du projet" />
             <p>{props.object.project_context}</p>
+            {displayPtech}
+            {displayPfunc}
         </div>
         
     )
