fcb431d8408005298786e2964dd06aaef814846a
Adrien-Ray <forpub@accesdenied.net>
2023-03-20 12:31:00 +0100

    add css animation to opening

--- src/components/Presentation/Presentation.jsx
+++ src/components/Presentation/Presentation.jsx
@@ -6,6 +6,7 @@ function Presentation() {
             <div className="presentation__center">
                 <h1>Bienvenu sur mon portfolio</h1>
                 <p>Ceci est un client web connecter à l'API du portfolio initial.</p>
+                <div className="presentation__center__layer"></div>
             </div>
         </div>
     );
